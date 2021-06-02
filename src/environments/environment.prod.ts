eac2e15da182b05eed59440a8b054c29a3ae3398
Daniel <daniel@digitlab.co.za>
2021-06-02 16:22:27 +0200

    firebase API key added, added the other users

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '',
+    apiKey: 'AIzaSyB6tp6MXS3e1HHIE2QrYMXgHwGwUM-N7to',
     authDomain: 'dispach-todo.firebaseapp.com',
     projectId: 'dispach-todo',
     storageBucket: 'dispach-todo.appspot.com',
