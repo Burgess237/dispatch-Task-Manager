ec95f12d6fa06a3bdf353f7e697bac54493ba77a
Daniel Burgess <burgess237@gmail.com>
2021-06-08 16:07:11 +0200

    First Attempt at adding google maps

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-
 import { AuthService } from '../services/auth.service';
 import { FirebaseService } from '../services/firebase.service';
 import { GeolocationService, LocationObject } from '../services/geolocation.service';
 import { User } from '../services/user';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
-import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { NativeGeocoder} from '@ionic-native/native-geocoder/ngx';
 import { Platform } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
 
 
 declare let google;
@@ -21,14 +19,15 @@ declare let google;
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
 
 
+  center: google.maps.LatLngLiteral = {lat: -29.772107899999998, lng: 30.999091999999997};
+  zoom = 7;
+  myMarker;
+  markers = [];
   liveposition: LocationObject;
-
-  height = 0;
-  latitude;
-  longitude;
-  markers: Marker[];
+  singleMarker: google.maps.Marker;
 
   constructor(
     private geolocation: Geolocation,
@@ -43,10 +42,24 @@ export class MapPage implements OnInit {
    async ngOnInit() {
      this.platform.ready().then(() => {
       this.fetchUserLocations();
-      this.height = this.platform.height() - 56;
       this.geolocation.getCurrentPosition().then((res: Geoposition) => {
-        this.latitude = res.coords.latitude;
-        this.longitude = res.coords.longitude;
+        console.log('Current: ', res);
+        this.center = {lat: res.coords.latitude, lng: res.coords.longitude};
+        this.myMarker = {
+          position: {
+            lat: res.coords.latitude,
+            lng: res.coords.longitude,
+          },
+          label: {
+            color: 'red',
+            text: 'You Are here!',
+          },
+          title: 'You Are here!',
+          info: 'You Are here!',
+          options: {
+            animation: google.maps.Animation.DROP,
+          }
+        };
       });
     });
 }
@@ -54,9 +67,20 @@ export class MapPage implements OnInit {
 fetchUserLocations() {
   this.firebaseService.getUsersForMap().subscribe((res: any) => {
     if(res){
-      this.markers = res.map(e=> ({
-        lng: e.payload.doc.data().lng,
-        lat: e.payload.doc.data().lat
+      this.markers = res.map(e=> ( {
+        position: {
+          lat: e.payload.doc.data().lat,
+          lng: e.payload.doc.data().lng,
+        },
+        label: {
+          color: 'red',
+          text: 'Marker label ' + (this.markers.length + 1),
+        },
+        title: 'Marker title ' + (this.markers.length + 1),
+        info: 'Marker info ' + (this.markers.length + 1),
+        options: {
+          animation: google.maps.Animation.DROP,
+        }
       })
       );
     }
@@ -66,11 +90,6 @@ fetchUserLocations() {
 
 
 
-}
 
-interface Marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
+
 }
